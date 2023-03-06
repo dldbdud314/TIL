5e9fd05e8bba7b85196129f6472a2a8384cd7d9e
dldbdud314 <dldbdud314@naver.com>
2023-03-06 22:24:29 +0900

    클래스에 프록시 적용하기_CGLIB, ByteBuddy

--- Java/codes/src/test/java/proxy/BookServiceTest.java
+++ Java/codes/src/test/java/proxy/BookServiceTest.java
@@ -1,10 +1,17 @@
 package proxy;
 
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 
 
 class BookServiceTest {
@@ -28,7 +35,7 @@ class BookServiceTest {
 
                             @Override
                             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                                if (method.getName().equals("rent")){
+                                if (method.getName().equals("rent")) {
                                     System.out.println("------log start------");
                                     Object invoke = method.invoke(realService, args);
                                     System.out.println("------log end------");
@@ -41,6 +48,54 @@ class BookServiceTest {
                         });
         System.out.println(proxyService.returnBook());
 
+        Book book = new Book();
+        book.setTitle("real log printed");
+        proxyService.rent(book);
+    }
+
+    @Test
+    @DisplayName("CGLIB로 클래스 기반의 프록시 만들기")
+    public void cglib() {
+        MethodInterceptor handler = new MethodInterceptor() {
+            DefaultBookService realService = new DefaultBookService();  // 구체클래스로 바꿔도 동작
+
+            @Override
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy)
+                    throws Throwable {
+                if (method.getName().equals("rent")) {
+                    System.out.println("******log start******");
+                    Object invoke = method.invoke(realService, args);
+                    System.out.println("******log end******");
+
+                    return invoke;
+                }
+
+                return method.invoke(realService, args);
+            }
+        };
+        BookService proxyService = (BookService) Enhancer.create(BookService.class, handler);
+        Book book = new Book();
+        book.setTitle("real log printed");
+        proxyService.rent(book);
+    }
+
+    @Test
+    @DisplayName("바이트버디 활용(바이트코드 조작) 프록시 만들기")
+    public void byteBuddy() throws Exception {
+        Class<? extends BookService> proxyClass = new ByteBuddy().subclass(BookService.class)
+                .method(named("rent")).intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+                    DefaultBookService realService = new DefaultBookService();
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("//////log start//////");
+                        Object invoke = method.invoke(realService, args);
+                        System.out.println("//////log end//////");
+                        return invoke;
+                    }
+                }))
+                .make().load(BookService.class.getClassLoader()).getLoaded();
+
+        BookService proxyService = proxyClass.getConstructor(null).newInstance();
         Book book = new Book();
         book.setTitle("real log printed");
         proxyService.rent(book);
